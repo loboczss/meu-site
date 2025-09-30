4e66ef914262e709f7c6c1cf240414a09260c94b
Juan <juanmproducoes@gmail.com>
2025-09-30 11:04:45 -0300

    feat: página Sobre

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function Sobre() {
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-semibold">Contato</h1>
+      <p className="mt-4">Página de contato funcionando ✅</p>
+    </main>
+  );
+}
